8e4fb570ff91e8cb460cf140cef350ee8d74a850
dgeoiacl <dgeoiacl@users.noreply.github.com>
2026-07-19 20:09:43 -0400

    chore: remove unused files not referenced by index.html

--- style_ex/stylephotobooth/src/imports/Rectangle2.tsx
+++
@@ -1,3 +0,0 @@
-export default function Rectangle() {
-  return <div className="bg-[#f6d0ef] size-full" />;
-}
--- style_ex/stylephotobooth/src/imports/Rectangle3.tsx
+++
@@ -1,3 +0,0 @@
-export default function Rectangle() {
-  return <div className="bg-[#c0e2e8] size-full" />;
-}
